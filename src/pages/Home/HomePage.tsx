f24fecc6f03b23a93e56f83199f00ca14bd922b1
= <=>
2021-12-28 11:15:28 +0200

    made Home and Statistic to classes components

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,18 +1,19 @@
+import React from 'react'
 import { Button } from '../../components/Button/Button'
 import { Header } from '../../components/Header/Header'
 import { Title } from '../../components/Title'
 import styles from './HomePage.module.sass'
 
-export const HomePage = () => {
-	return (
-        <div className={styles.content}>
-            <div className={styles.content_header}>
-                <Header showArrow={false} />
-                <Title title="Jane's Profile" subTitle='11 tasks' />
-		    </div>
-            <div>
-                <Button />
-            </div>
+export class HomePage extends React.Component {
+	render() {
+       return  <div className={styles.content}>
+        <div className={styles.content_header}>
+            <Header showArrow={false} />
+            <Title title="Jane's Profile" subTitle='11 tasks' />
         </div>
-	)
+        <div>
+            <Button />
+        </div>
+    </div>
+    }
 }
