8199777f03772727e81da34ec57c44ac99580a15
= <=>
2022-01-05 13:24:56 +0200

    refactor styles

--- src/pages/Statistic/StatisticPage.tsx
+++ src/pages/Statistic/StatisticPage.tsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import { Button } from '../../components/Button'
 import { Header } from '../../components/layout/Header'
+import styles from './StatisticPage.module.sass'
 
 export class StatisticPage extends React.Component {
 	render() {
 		return (
-			<>
+			<div className={styles.content}>
 				<Header title={`Hey Jane,\nThese are your statistics.`} canGoBack={true} />
 				<Button showButton={false} />
-			</>
+			</div>
 		)
 	}
 }
