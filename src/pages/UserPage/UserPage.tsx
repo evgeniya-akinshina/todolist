3b6a2ffbb54134036ae878c3a1ac94b9d55d4053
= <=>
2022-01-31 17:47:51 +0200

    fixed UsersPage and UserPage

--- src/pages/UserPage/UserPage.tsx
+++ src/pages/UserPage/UserPage.tsx
@@ -4,44 +4,32 @@ import { Header } from '../../components/layout/Header'
 import styles from './UserPage.module.sass'
 import { Props } from './types'
 import { useParams } from 'react-router-dom'
+import { PageContent } from '../../components/layout/PageContent'
+import find from 'lodash/find'
+import { User } from '../../store/reducers/users'
 
 export const UserPage = (props: Props) => {
 	const { users } = props
 	const { id } = useParams()
+	const user = find(users, { id: Number(id) }) as User
+	// users.find(user => user.id === id)
 	return (
 		<>
 			{users.length > 0 && (
 				<div className={styles.content}>
-					{id === '1' ? (
-						<Header title={'User ' + users[0].id} subTitle={users[0].todos.length + ' todos'} canGoBack />
-					) : null}
-					{id === '2' ? (
-						<Header title={'User ' + users[1].id} subTitle={users[1].todos.length + ' todos'} canGoBack />
-					) : null}
-					{id === '3' ? (
-						<Header title={'User ' + users[2].id} subTitle={users[2].todos.length + ' todos'} canGoBack />
-					) : null}
-               {id === '4' ? (
-						<Header title={'User ' + users[3].id} subTitle={users[3].todos.length + ' todos'} canGoBack />
-					) : null}
-               {id === '5' ? (
-						<Header title={'User ' + users[4].id} subTitle={users[4].todos.length + ' todos'} canGoBack />
-					) : null}
-               {id === '6' ? (
-						<Header title={'User ' + users[5].id} subTitle={users[5].todos.length + ' todos'} canGoBack />
-					) : null}
-               {id === '7' ? (
-						<Header title={'User ' + users[6].id} subTitle={users[6].todos.length + ' todos'} canGoBack />
-					) : null}
-               {id === '8' ? (
-						<Header title={'User ' + users[7].id} subTitle={users[7].todos.length + ' todos'} canGoBack />
-					) : null}
-               {id === '9' ? (
-						<Header title={'User ' + users[8].id} subTitle={users[8].todos.length + ' todos'} canGoBack />
-					) : null}
-               {id === '10' ? (
-						<Header title={'User ' + users[9].id} subTitle={users[9].todos.length + ' todos'} canGoBack />
-					) : null}
+					<Header title={user.name} subTitle={user.todos.length + ' todos'} canGoBack />
+					<PageContent>
+						{user.todos.map(todo => {
+							return (
+								<>
+									<p>{`${todo.id}`}</p>
+									{/* how make numeration only before 20 */}
+									<p>{`${todo.createAt.toDateString()}`}</p>
+									<p>{`${todo.title}`}</p>
+								</>
+							)
+						})}
+					</PageContent>
 					<Button className={styles.button} onClick={() => (window.location.href = '/add-new-tasks')} showButton>
 						+ Add new task
 					</Button>
--- src/pages/UsersPage/UsersPage.tsx
+++ src/pages/UsersPage/UsersPage.tsx
@@ -14,95 +14,17 @@ export class UsersPage extends React.Component<Props> {
 				<PageContent>
 					{users.length > 0 && (
 						<div className={styles.menu}>
-							<div className={styles.menuItem}>
-								<MenuCard
-									activeLink
-									title={'User ' + users[0].id}
-									link={`/user/${users[0].id}`}
-									subTitle={users[0].todos.length + ' todos'}
-								/>
-							</div>
-
-							<div className={styles.menuItem}>
-								<MenuCard
-									activeLink
-									title={'User ' + users[1].id}
-									link={`/user/${users[1].id}`}
-									subTitle={users[1].todos.length + ' todos'}
-								/>
-							</div>
-
-							<div className={styles.menuItem}>
-								<MenuCard
-									activeLink
-									title={'User ' + users[2].id}
-									link={`/user/${users[2].id}`}
-									subTitle={users[2].todos.length + ' todos'}
-								/>
-							</div>
-
-							<div className={styles.menuItem}>
-								<MenuCard
-									activeLink
-									title={'User ' + users[3].id}
-									link={`/user/${users[3].id}`}
-									subTitle={users[3].todos.length + ' todos'}
-								/>
-							</div>
-
-							<div className={styles.menuItem}>
-								<MenuCard
-									activeLink
-									title={'User ' + users[4].id}
-									link={`/user/${users[4].id}`}
-									subTitle={users[4].todos.length + ' todos'}
-								/>
-							</div>
-
-							<div className={styles.menuItem}>
-								<MenuCard
-									activeLink
-									title={'User ' + users[5].id}
-									link={`/user/${users[5].id}`}
-									subTitle={users[5].todos.length + ' todos'}
-								/>
-							</div>
-
-							<div className={styles.menuItem}>
-								<MenuCard
-									activeLink
-									title={'User ' + users[6].id}
-									link={`/user/${users[6].id}`}
-									subTitle={users[6].todos.length + ' todos'}
-								/>
-							</div>
-
-							<div className={styles.menuItem}>
-								<MenuCard
-									activeLink
-									title={'User ' + users[7].id}
-									link={`/user/${users[7].id}`}
-									subTitle={users[7].todos.length + ' todos'}
-								/>
-							</div>
-
-							<div className={styles.menuItem}>
-								<MenuCard
-									activeLink
-									title={'User ' + users[8].id}
-									link={`/user/${users[8].id}`}
-									subTitle={users[8].todos.length + ' todos'}
-								/>
-							</div>
-
-							<div className={styles.menuItem}>
-								<MenuCard
-									activeLink
-									title={'User ' + users[9].id}
-									link={`/user/${users[9].id}`}
-									subTitle={users[9].todos.length + ' todos'}
-								/>
-							</div>
+							{this.props.users.map((item, index) => (
+								<div className={styles.menuItem}>
+									<MenuCard
+										activeLink
+										key={index}
+										title={'User ' + item.id}
+										link={`/user/${item.id}`}
+										subTitle={item.todos.length + ' todos'}
+									/>
+								</div>
+							))}
 						</div>
 					)}
 				</PageContent>
