4c3805cab24f789edd3e75a659e1e332e4a25867
= <=>
2022-02-04 23:29:32 +0200

    fixed layout

--- src/components/MenuCard/MenuCard.tsx
+++ src/components/MenuCard/MenuCard.tsx
@@ -7,11 +7,15 @@ export const MenuCard = ({ title, subTitle, to, progress }: Props) => {
 	return (
 		<Link to={to} className={styles.container}>
 			{progress && (
-				<div className={styles.progress} style={{ width: `${(progress.value / progress.value) * 100}%` }} />
+				<div className={styles.progress} style={{ width: `${(progress.value / progress.total) * 100}%` }} />
 			)}
-			<h3>{title[0]}</h3>
-			<h3 className={styles.title}>{title}</h3>
-			{subTitle && <p>{subTitle}</p>}
+			<div className={styles.card}>
+				<h3 className={styles.letter}>{title[0]}</h3>
+				<div className={styles.content}>
+					<h3 className={styles.title}>{title}</h3>
+					{subTitle && <p>{subTitle}</p>}
+				</div>
+			</div>
 		</Link>
 	)
 }
